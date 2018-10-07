ce37198f64de43bc4ff5035d27229426e0d1ca0e
castilloej <castilloej@email.wofford.edu>
2018-10-07 19:35:49 -0400

    Made the terminal version of the game work

--- src/main/java/edu/wofford/ConsoleMain.java
+++ src/main/java/edu/wofford/ConsoleMain.java
@@ -5,7 +5,24 @@ import java.util.Scanner;
 public class ConsoleMain {
 
 	public static void main(String[] args) {
-		Scanner keyboard = new Scanner(System.in);
+		TicTacToeModel theGame = new TicTacToeModel();
 
+		Scanner scanner = new Scanner(System.in);
+
+		while(!theGame.IsGameOver()){
+
+				System.out.println("-----------");
+				System.out.println("Game Status:\n");
+				System.out.println(theGame.toString());
+				System.out.print("\nSet next mark \"row col\": ");
+
+				Scanner subScanner = new Scanner(scanner.nextLine());
+
+				theGame.setMarkAt(subScanner.nextInt(), subScanner.nextInt());
+
+		}
+
+		System.out.println("\n" + theGame.toString() + "\n");
+		System.out.println(theGame.getResultStr());
 	}
 }
